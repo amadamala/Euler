0a4c497a77c2b361742bf84b3dbf316b58dcd38a
Anil <amadamala@gmail.com>
2011-05-06 01:20:50 +0530

    lazily calling isAbundant method

--- Python/Problem23.py
+++ Python/Problem23.py
@@ -1,7 +1,7 @@
 """
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 """
-abundants = []
+abundants = {}
 
 """
 It can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
@@ -17,11 +17,25 @@ def isAbundant(n):
 		return True
 	# print sum
 
+def sum_numbers_cannot_written_as_sum_of_2_abundants():
+	sum = 0
+	for i in range(12, UPPER_LIMIT):
+		for j in range(12, i/2):
+			n1 = j
+			n2 = i - j
+			if isAbundant(n1) and isAbundant(n2):
+				print i, n1, n2
+				break;
+		else:
+			sum += i				
+	print sum			
+
 def main():
-	for i in range(12, UPPER_LIMIT+1):
-		if isAbundant(i):
-			abundants.append(i)
-		print abundants		
-	
+	# for i in range(12, UPPER_LIMIT/2):
+	# 	if isAbundant(i):
+	# 		abundants[i] = i
+	# print abundants		
+	sum_numbers_cannot_written_as_sum_of_2_abundants()
+
 if __name__ == "__main__":
     main()
