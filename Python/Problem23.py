23b340821ced300660641fbc1e40bdb8f0237d1e
Anil <amadamala@gmail.com>
2011-05-05 20:51:20 +0530

    generated all the abundant numbers

--- Python/Problem23.py
+++ Python/Problem23.py
@@ -1,13 +1,28 @@
 """
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 """
-a = []
+abundants = []
 
-def proper_divisors(n):
-	print n
+"""
+It can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
+"""
+UPPER_LIMIT = 28123
+
+def isAbundant(n):
+	sum = 0
 	for i in range(1, n/2 + 1):
 		if n % i == 0:
-			a.append(i)
-	print a
+			sum += i
+	if sum > n :
+		return True
+	# print sum
 
-proper_divisors(10)		
+def main():
+	for i in range(12, UPPER_LIMIT+1):
+		if isAbundant(i):
+			abundants.append(i)
+	print abundants		
+	
+if __name__ == "__main__":
+    main()
+
