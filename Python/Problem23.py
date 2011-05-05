3429b4ad6c2c96b8400f55fb3f3033973b92d94f
Anil <amadamala@gmail.com>
2011-05-06 02:02:42 +0530

    using dictionary to store abundants.

--- Python/Problem23.py
+++ Python/Problem23.py
@@ -1,12 +1,13 @@
+from collections import defaultdict
 """
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 """
-abundants = {}
+abundants = defaultdict(int)
 
 """
 It can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
 """
-UPPER_LIMIT = 28123
+LIMIT = 28123
 
 def isAbundant(n):
 	sum = 0
@@ -19,21 +20,24 @@ def isAbundant(n):
 
 def sum_numbers_cannot_written_as_sum_of_2_abundants():
 	sum = 0
-	for i in range(12, UPPER_LIMIT):
-		for j in range(12, i/2):
+	for i in range(1, LIMIT):
+		j = 12
+		while j < (i/2 + 1):
 			n1 = j
 			n2 = i - j
-			if isAbundant(n1) and isAbundant(n2):
-				print i, n1, n2
-				break;
-		else:
-			sum += i				
+			if (n1 in abundants) and (n2 in abundants):
+				# print i, n1, n2
+				break
+			j += 1
+		if j != i/2:
+			# print i
+			sum += i
 	print sum			
 
 def main():
-	# for i in range(12, UPPER_LIMIT/2):
-	# 	if isAbundant(i):
-	# 		abundants[i] = i
+	for i in range(12, (LIMIT/2 + 1)):
+		if isAbundant(i):
+			abundants[i] = i
 	# print abundants		
 	sum_numbers_cannot_written_as_sum_of_2_abundants()
 
