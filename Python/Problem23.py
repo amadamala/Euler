de9076ff354dc7a9190f748b43ab0d6f848ed3af
Anil <amadamala@gmail.com>
2011-05-06 19:21:12 +0530

    improvised the speed of the program

--- Python/Problem23.py
+++ Python/Problem23.py
@@ -1,28 +1,44 @@
-from collections import defaultdict
 """
-It can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+It can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
 """
-abundants = defaultdict(int)
-LIMIT = 28123
 
-def isAbundant(n):
-	sum = 0
-	for i in range(1, n/2 + 1):
-		if n % i == 0:
-			sum += i
-	if sum > n :
-		return True
-	# print sum
+from time import *
+from collections import defaultdict
+
+LIMIT = 28123
+abundants = [None] * LIMIT
+
+
+# def isAbundant(n):
+# 	sum = 0
+# 	for i in range(1, n/2 + 1):
+# 		if n % i == 0:
+# 			sum += i
+# 	# print n, sum		
+# 	if sum > n :
+# 		return True
+
+def d(n):
+	for i in range(1, n + 1):
+		j = 2
+		while (i * j) < n:
+			if abundants[i * j] is None:
+				abundants[i * j] = i
+			else:
+				abundants[i * j] += i
+			j += 1
+	# for i in range(len(abundants)):
+	# 	print i, abundants[i]
 
 def sum_numbers_cannot_written_as_sum_of_2_abundants():
 	sum = (LIMIT * (LIMIT + 1) ) / 2
-	
+
 	for i in range(1, LIMIT + 1):
 		for j in range(1, i/2 + 1):
 			n1 = j
 			n2 = i - j
-			if (n1 in abundants) and (n2 in abundants):
+			if (n1 < abundants[n1]) and (n2 < abundants[n2]):
 				# print i, n1, n2
 				sum = sum - i;
 				break
@@ -30,11 +46,13 @@ def sum_numbers_cannot_written_as_sum_of_2_abundants():
 	print sum			
 
 def main():
-	for i in range(12, LIMIT):
-		if isAbundant(i):
-			abundants[i] = i
+	time_taken = clock()
+	d(LIMIT)
+	# time_taken = clock() - time_taken
+	# print "Time taken = %.2f" % (clock() - time_taken)		
 	# print abundants		
 	sum_numbers_cannot_written_as_sum_of_2_abundants()
+	print "Time taken = %.2f" % (clock() - time_taken)		
 
 if __name__ == "__main__":
     main()
