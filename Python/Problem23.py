9576a37a52f32375e48a7a822d8932fc9923dda3
Anil <amadamala@gmail.com>
2011-05-07 15:44:35 +0530

    By knowing the fact "All even numbers greater than 48 can be written as the sum of two abundant" changed the algorithm. Time taken reduced from 6 sec 4 sec.

--- Python/Problem23.py
+++ Python/Problem23.py
@@ -2,10 +2,18 @@
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 It can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
 """
+
+"""
+
+The upper bound for numbers that cannot be expressed as the sum of two numbers is actually
+20161 not 28123 (as shown here). 
+All even numbers greater than 48 can be written as the sum of two abundant numbers 
+Minimum abundant not multiple of 2 or 3 = 5391411025
+"""
 from time import *
 from collections import defaultdict
 
-LIMIT = 28123
+LIMIT = 20161
 abundants = [None] * LIMIT
 
 def d(n):
@@ -19,22 +27,25 @@ def d(n):
 			j += 1
 
 def sum_numbers_cannot_written_as_sum_of_2_abundants():
-	sum = (LIMIT * (LIMIT + 1) ) / 2
-
+	sum = LIMIT * (LIMIT + 1)  / 2
 	for i in range(1, LIMIT + 1):
-		for j in range(1, i/2 + 1):
-			n1 = j
-			n2 = i - j
-			if (n1 < abundants[n1]) and (n2 < abundants[n2]):
-				sum = sum - i;
-				break
-
-	print sum			
-
+		# All even numbers greater than 48 can be written as the sum of two abundant numbers 
+		if i > 48 and i % 2 == 0:
+			sum = sum - i
+		else:		
+			for j in range(1, i/2 + 1): 
+				n1 = j
+				n2 = i - j
+				if (n1 < abundants[n1]) and (n2 < abundants[n2]):
+					sum = sum - i;
+					break
+	print sum	
+# 4179871
 def main():
 	time_taken = clock()
 	d(LIMIT)		
 	sum_numbers_cannot_written_as_sum_of_2_abundants()
+	# solution()
 	print "Time taken = %.2f" % (clock() - time_taken)		
 
 if __name__ == "__main__":
