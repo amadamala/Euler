15a85a5f727ac1b6b9e52bd13831f2a410bbdf76
Anil <amadamala@gmail.com>
2011-05-12 00:54:00 +0530

    final solution.

--- Python/Problem41.py
+++ Python/Problem41.py
@@ -1,5 +1,6 @@
 from math import sqrt
 from time import *
+
 def isprime(n):
 	prime = True
 	for i in range(3, int(sqrt(n)) + 1, 2):
@@ -11,41 +12,31 @@ def isprime(n):
 	else:
 		return False
 
-# def perm(s):
-# 	perm1('', s)
-def perm(prefix , s):
+permutations = []
+# Method to generate all permutations
+def perm(s, prefix = ""):
 	N = len(s)
 	if N == 0:
-		if int(prefix) % 2 != 0:
-			if isprime(int(prefix)) and ispandigital(int(prefix)):
-				print prefix
+		p = int(prefix)
+		if p % 2 != 0:
+			if isprime(p) and ispandigital(p):
+				permutations.append(prefix)
 	else:
 		for i in range(N):
-			perm((prefix + s[i]), (s[0:i] + s[i+1:N]))
-               # perm1(prefix + s.charAt(i), s.substring(0, i) + s.substring(i+1, N));
-				
-def foo(n):
-	for i in range(n, 1, -1):
-		if isprime(i) and ispandigital(i):
-			print i
-
+			perm((s[0:i] + s[i+1:N]), (prefix + s[i]))
+			
 def ispandigital(n):
 	l = list(str(n))
 	l.sort()
 	for i in range(len(l)):
 		if (i+1) != int(l[i]):
 			return False
-	# print n
 	return True
 
 def main():
 	t = clock()
-	foo(1234567)
-	print "Time taken: %0.2f" %(clock() - t)
-	
-	t = clock()
-	
-	perm("", '1234567')
+	perm('1234567')
+	print max(permutations)
 	print "Time taken: %0.2f" %(clock() - t)
 	
 if __name__ == '__main__':
